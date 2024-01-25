0caf3eeba7f86233b2412405c44e0a95b683a2aa
Thiago Luis Trigueiro <thiagoluis@unipam.edu.br>
2024-01-25 17:40:53 -0300

    atualizando a tasksModels para realizar um query no banco de dados

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -4,7 +4,20 @@ const getAll = async() => {
     const [tasks] = await connection.execute('SELECT * FROM tasks');
     return tasks;
 };
+
+const createTask = async(task) => {
+    
+    const { title } = task;
+
+    const dateUTC = new Date(Date.now()).toUTCString();
+
+    const createdTask = await connection,execute('INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)' [1, 'pendente', dateUTC]);
+
+    return createdTask;
+
+}
  
 module.exports = {
-    getAll
+    getAll,
+    createTask
 }
