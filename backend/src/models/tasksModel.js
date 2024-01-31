810ba49385cff30518f89e2ec83812539e23cb9d
Thiago Luis Trigueiro <thiagoluis@unipam.edu.br>
2024-01-31 11:51:29 -0300

    backend de tasks funcionando

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,23 +1,34 @@
 const connection = require('./connection');
 
-const getAll = async() => {
-    const [tasks] = await connection.execute('SELECT * FROM tasks');
-    return tasks;
+const getAll = async () => {
+  const [tasks] = await connection.execute('SELECT * FROM tasks');
+  return tasks;
 };
 
-const createTask = async(task) => {
-    
-    const { title } = task;
+const createTask = async (task) => {
+  const { title } = task;
+  const dateUTC = new Date(Date.now()).toLocaleDateString();
+  const query = 'INSERT INTO tasks(title,status,created) VALUES (?,?,?)';
 
-    const dateUTC = new Date(Date.now()).toUTCString();
+ const [createdTask] = await connection.execute(query, [title,'pendente',dateUTC]);
+ return {insertId: createdTask.insertId};
+};
 
-    const createdTask = await connection,execute('INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)' [1, 'pendente', dateUTC]);
+const deleteTask = async (id) => {
+  const removedTask = await connection.execute('DELETE FROM tasks WHERE id = ?', [id]);
+  return removedTask;
+}
 
-    return createdTask;
+const updateTask = async (id, task) => {
+  const { title, status } = task;
 
+  const updatedTask = await connection.execute('UPDATE tasks SET title = ?, status = ? WHERE id = ?', [title, status, id]);
+  return updatedTask;
 }
  
 module.exports = {
     getAll,
-    createTask
+    createTask,
+    deleteTask,
+    updateTask,
 }
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const  tasksController = require('./controllers/tasksController');
 
 const router = express.Router();
 
+const tasksController = require('./controllers/tasksController');
+const tasksMiddleware = require('./middlewares/tasksMiddleware');
 
 router.get('/tasks', tasksController.getAll);
-
+router.post('/tasks', tasksMiddleware.validateBody, tasksController.createTask);
+router.delete('/tasks/:id', tasksController.deleteTask);
+router.put('/tasks/:id',tasksMiddleware.validateBody,tasksMiddleware.validadeStatus, tasksController.updateTask);
 
 module.exports = router;
